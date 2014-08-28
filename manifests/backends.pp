3fb439e342fbf1606ec48da40166498f6d72273f
Ryan Hefner <ryan.hefner@netapp.com>
2014-08-27 20:06:07 -0400

    change enabled_backends to enabled_share_backends

--- manifests/backends.pp
+++ manifests/backends.pp
@@ -11,12 +11,12 @@
 #
 # Author: Andrew Woodward <awoodward@mirantis.com>
 class manila::backends (
-  $enabled_backends    = undef
+  $enabled_share_backends    = undef
   ){
 
   # Maybe this could be extented to dynamicly find the enabled names
   manila_config {
-    'DEFAULT/enabled_share_backends': value => join($enabled_backends, ',');
+    'DEFAULT/enabled_share_backends': value => join($enabled_share_backends, ',');
   }
 
 }
