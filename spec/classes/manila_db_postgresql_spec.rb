cca9f888bf8681474830c405d2737b0331a09549
ZhongShengping <chdzsp@163.com>
2016-04-30 11:31:40 +0800

    Test multiple operating systems for manila::db::postgresql
    Change-Id: I21dce1d970695c5ee39f4038a9eb66a058feb538

--- spec/classes/manila_db_postgresql_spec.rb
+++ spec/classes/manila_db_postgresql_spec.rb
@@ -2,21 +2,13 @@ require 'spec_helper'
 
 describe 'manila::db::postgresql' do
 
-  let :req_params do
-    { :password => 'pw' }
-  end
+  shared_examples_for 'manila::db::postgresql' do
+    let :req_params do
+      { :password => 'pw' }
+    end
 
-  let :pre_condition do
-    'include postgresql::server'
-  end
-
-  context 'on a RedHat osfamily' do
-    let :facts do
-      @default_facts.merge({
-        :osfamily                 => 'RedHat',
-        :operatingsystemrelease   => '7.0',
-        :concat_basedir => '/var/lib/puppet/concat'
-      })
+    let :pre_condition do
+      'include postgresql::server'
     end
 
     context 'with only required parameters' do
@@ -32,27 +24,19 @@ describe 'manila::db::postgresql' do
 
   end
 
-  context 'on a Debian osfamily' do
-    let :facts do
-      @default_facts.merge({
-        :operatingsystemrelease => '7.8',
-        :operatingsystem        => 'Debian',
-        :osfamily               => 'Debian',
-        :concat_basedir => '/var/lib/puppet/concat'
-      })
-    end
-
-    context 'with only required parameters' do
-      let :params do
-        req_params
+  on_supported_os({
+    :supported_os   => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge(OSDefaults.get_facts({
+          :processorcount => 8,
+          :concat_basedir => '/var/lib/puppet/concat'
+        }))
       end
 
-      it { is_expected.to contain_postgresql__server__db('manila').with(
-        :user     => 'manila',
-        :password => 'md5b15a47e3dd847f86827b328466426faf'
-      )}
+      it_configures 'manila::db::postgresql'
     end
-
   end
 
 end
