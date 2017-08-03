97780108740a1e8dc92944e4bd2532b80e853a27
ZhongShengping <chdzsp@163.com>
2017-08-03 10:09:39 +0800

    Fix unit test for manila::client class
    Change-Id: I5727d8cd2d01e6ae6c75fb45dc189dd93fed71e8

--- spec/classes/manila_client_spec.rb
+++ spec/classes/manila_client_spec.rb
@@ -2,38 +2,34 @@ require 'spec_helper'
 
 describe 'manila::client' do
 
-  let :params do
-    {}
-  end
-
-  let :default_params do
-    { :package_ensure   => 'present' }
-  end
-
   shared_examples_for 'manila client' do
-    let :p do
-      default_params.merge(params)
-    end
 
+    it { is_expected.to contain_class('manila::deps') }
     it { is_expected.to contain_class('manila::params') }
-  
+
     it 'installs manila client package' do
-      it is_expected.to contain_package('python-manilaclient').with(
-        :name   => 'python-manilaclient',
-        :ensure => p[:package_ensure],
-        :tag    => ['openstack', 'manila-support-package'],
+      is_expected.to contain_package('python-manilaclient').with(
+        :ensure => 'present',
+        :name   => platform_params[:client_package],
+        :tag    => ['openstack', 'manila-support-package']
       )
     end
-
   end
 
-  let :facts do
-    @default_facts.merge({:osfamily => 'Debian'})
-  end
-  context 'with params' do
-    let :params do
-      {:package_ensure => 'latest'}
+  on_supported_os({
+    :supported_os => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge!(OSDefaults.get_facts())
+      end
+
+      let :platform_params do
+        { :client_package => 'python-manilaclient' }
+      end
+
+      it_behaves_like 'manila client'
     end
-    it { is_expected.to contain_package('python-manilaclient').with_ensure('latest') }
   end
+
 end
