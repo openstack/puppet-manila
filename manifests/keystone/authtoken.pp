cfb4554899057696a5bb93f2b610292925c1b365
ZhongShengping <chdzsp@163.com>
2017-09-21 09:15:25 +0800

    Add deps to all that is needed
    Change-Id: I68b2c5d4485987211603991470a328f0cc82638f
    (cherry picked from commit 04f0ebf30a6d4ff383489362707f33ea6a82aca0)

--- manifests/keystone/authtoken.pp
+++ manifests/keystone/authtoken.pp
@@ -223,6 +223,8 @@ class manila::keystone::authtoken(
   $token_cache_time               = $::os_service_default,
 ) {
 
+  include ::manila::deps
+
   if is_service_default($password) {
     fail('Please set password for manila service user')
   }
