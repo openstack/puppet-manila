773c72ea00fa8eb48a4e697eb3915aeaafe16fd1
Juan Antonio Osorio Robles <jaosorior@redhat.com>
2017-10-06 13:00:17 +0300

    Accept empty strings for log_dir
    An empty string is an acceptable value of this entry, and it forces
    logging to stdout/stderr, which is useful when running on containers.
    
    Change-Id: Idbe00cc844f9d1d088d5b55c9c0cf76647de10ab

--- manifests/logging.pp
+++ manifests/logging.pp
@@ -111,7 +111,11 @@ class manila::logging(
   $use_syslog_real = pick($::manila::use_syslog,$use_syslog)
   $use_stderr_real = pick($::manila::use_stderr,$use_stderr)
   $log_facility_real = pick($::manila::log_facility,$log_facility)
-  $log_dir_real = pick($::manila::log_dir,$log_dir)
+  if $log_dir != '' {
+    $log_dir_real = pick($::manila::log_dir,$log_dir)
+  } else {
+    $log_dir_real = $log_dir
+  }
   $debug_real = pick($::manila::debug,$debug)
 
   oslo::log { 'manila_config':
