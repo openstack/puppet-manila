5ae8e2882b3e8229ea0007938315f5145e4f5f8a
David Vallee Delisle <dvd@redhat.com>
2018-11-08 17:09:45 -0500

    Updating nova and neutron configuration
    Since we stopped support of keystonev2, we need to be able to specify a
    domain in neutron and nova configuration. Might as well add all the
    options and remove the deprecated ones.
    
    Co-authored-by: Victoria Martinez de la Cruz <victoria@redhat.com>
    Change-Id: I745a170ac4458a3f13efc255fc37540a11b54274
    Closes-Bug: #1802393

--- manifests/compute/nova.pp
+++ manifests/compute/nova.pp
@@ -4,62 +4,170 @@
 #
 # === Parameters
 #
+# [*insecure*]
+#   (optional) Verify HTTPS connections
+#   Defaults to $::os_service_default
+#
+# [*auth_url*]
+#   (optional) Authentication URL
+#   Defaults to $::os_service_default
+#
+# [*auth_type*]
+#   (optional) Authentication type to load
+#   Defaults to $::os_service_default
+#
+# [*cafile*]
+#   (optional) PEM encoded Certificate Authority to use when verifying HTTPS
+#   connections.
+#   Defaults to $::os_service_default
+#
+# [*user_domain_name*]
+#   (optional) User's domain name
+#   Defaults to 'Default'
+#
+# [*project_domain_name*]
+#   (optional) Domain name containing project
+#   Defaults to 'Default'
+#
+# [*project_name*]
+#   (optional) Project name to scope to
+#   Defaults to 'service'
+#
+# [*region_name*]
+#   (optional) Region name for connecting to nova
+#   Defaults to $::os_service_default
+#
+# [*endpoint_type*]
+#   (optional) The type of nova endpoint to use when
+#   looking up in the keystone catalog.
+#   Defaults to $::os_service_default
+#
+# [*username*]
+#   (optional) Username
+#   Defaults to 'nova'
+#
+# [*password*]
+#   (optional) User's password
+#   Only required if auth_type has been set to "password"
+#   Defaults to undef
+#
+# === DEPRECATED PARAMETERS
+#
 # [*nova_catalog_info*]
 #   (optional) Info to match when looking for nova in the service
-#   catalog. Format is : separated values of the form:
+#   catalog. Format is: separated values of the form
 #   <service_type>:<service_name>:<endpoint_type>
-#   Defaults to 'compute:nova:publicURL'
+#   Defaults to undef
 #
 # [*nova_catalog_admin_info*]
-#   (optional) Same as nova_catalog_info, but for admin endpoint.
-#   Defaults to 'compute:nova:adminURL'
-#
-# [*nova_ca_certificates_file*]
-#   (optional) Location of ca certificates file to use for nova client
-#   requests.
+#   (optional) Same as nova_catalog_info, but for admin endpoint
 #   Defaults to undef
 #
 # [*nova_api_insecure*]
-#   (optional) Allow to perform insecure SSL requests to nova.
-#   Defaults to false
+#   Allow to perform insecure SSL requests to nova
+#   Defaults to undef
+#
+# [*nova_ca_certificates_file*]
+#   (optional) Location of CA certificates file to use for nova client requests
+#   (string value)
+#   Defaults to undef
 #
 # [*nova_admin_username*]
-#   (optional) Nova admin username.
-#   Defaults to 'nova'
+#   (optional) Nova admin username
+#   Defaults to undef
 #
 # [*nova_admin_password*]
-#   (optional) Nova admin password.
+#   (optional) Nova admin password
 #   Defaults to undef
 #
 # [*nova_admin_tenant_name*]
-#   (optional) Nova admin tenant name.
-#   Defaults to 'service'
+#   (optional) Nova admin tenant name
+#   Defaults to undef
 #
 # [*nova_admin_auth_url*]
-#  (optional) Identity service url.
-#   Defaults to 'http://localhost:5000/v2.0'
+#  (optional) Identity service url
+#   Defaults to undef
 #
 class manila::compute::nova (
-  $nova_catalog_info         = 'compute:nova:publicURL',
-  $nova_catalog_admin_info   = 'compute:nova:adminURL',
+  $insecure                  = $::os_service_default,
+  $auth_url                  = $::os_service_default,
+  $auth_type                 = $::os_service_default,
+  $cafile                    = $::os_service_default,
+  $user_domain_name          = 'Default',
+  $project_domain_name       = 'Default',
+  $project_name              = 'service',
+  $region_name               = $::os_service_default,
+  $endpoint_type             = $::os_service_default,
+  $username                  = 'nova',
+  $password                  = undef,
+  # DEPRECATED PARAMETERS
+  $nova_catalog_info         = undef,
+  $nova_catalog_admin_info   = undef,
+  $nova_api_insecure         = undef,
   $nova_ca_certificates_file = undef,
-  $nova_api_insecure         = false,
-  $nova_admin_username       = 'nova',
+  $nova_admin_username       = undef,
   $nova_admin_password       = undef,
-  $nova_admin_tenant_name    = 'service',
-  $nova_admin_auth_url       = 'http://localhost:5000/v2.0',
+  $nova_admin_tenant_name    = undef,
+  $nova_admin_auth_url       = undef,
 ) {
 
   include ::manila::deps
 
+  if $nova_catalog_info {
+    warning('The nova_catalog_info parameter is deprecated, has no effect and will be removed in a future release.')
+  }
+
+  if $nova_catalog_admin_info {
+    warning('The nova_catalog_admin_info parameter is deprecated, has no effect and will be removed in a future release.')
+  }
+
+  if $nova_api_insecure {
+    warning('The nova_api_insecure parameter is deprecated, use insecure instead.')
+  }
+
+  if $nova_ca_certificates_file {
+    warning('The nova_ca_certificates_file parameter is deprecated, use cafile instead.')
+  }
+
+  if $nova_admin_username {
+    warning('The nova_admin_username parameter is deprecated, use username instead.')
+  }
+
+  if $nova_admin_password {
+    warning('The nova_admin_password parameter is deprecated, use password instead.')
+  }
+
+  if $nova_admin_tenant_name {
+    warning('The nova_admin_tenant_name parameter is deprecated, use project_name instead.')
+  }
+
+  if $nova_admin_auth_url {
+    warning('The nova_admin_auth_url parameter is deprecated, use auth_url instead.')
+  }
+
+  $insecure_real = pick($nova_api_insecure, $insecure)
+  $cafile_real = pick($nova_ca_certificates_file, $cafile)
+  $username_real = pick($nova_admin_username, $username)
+  $password_real = pick_default($nova_admin_password, $password)
+  $project_name_real = pick($nova_admin_tenant_name, $project_name)
+  $auth_url_real = pick($nova_admin_auth_url, $auth_url)
+
   manila_config {
-    'DEFAULT/nova_catalog_info':         value => $nova_catalog_info;
-    'DEFAULT/nova_catalog_admin_info':   value => $nova_catalog_admin_info;
-    'DEFAULT/nova_ca_certificates_file': value => $nova_ca_certificates_file;
-    'DEFAULT/nova_api_insecure':         value => $nova_api_insecure;
-    'DEFAULT/nova_admin_username':       value => $nova_admin_username;
-    'DEFAULT/nova_admin_password':       value => $nova_admin_password, secret => true;
-    'DEFAULT/nova_admin_tenant_name':    value => $nova_admin_tenant_name;
-    'DEFAULT/nova_admin_auth_url':       value => $nova_admin_auth_url;
+    'nova/insecure':      value => $insecure_real;
+    'nova/auth_url':      value => $auth_url_real;
+    'nova/auth_type':     value => $auth_type;
+    'nova/cafile':        value => $cafile_real;
+    'nova/region_name':   value => $region_name;
+    'nova/endpoint_type': value => $endpoint_type;
     }
+
+  if $auth_type == 'password' {
+    manila_config {
+      'nova/username':            value => $username_real;
+      'nova/user_domain_name':    value => $user_domain_name;
+      'nova/password':            value => $password_real, secret => true;
+      'nova/project_name':        value => $project_name_real;
+      'nova/project_domain_name': value => $project_domain_name;
+    }
+  }
 }
--- spec/classes/manila_network_neutron_spec.rb
+++ spec/classes/manila_network_neutron_spec.rb
@@ -1,47 +1,88 @@
 require 'spec_helper'
 
 describe 'manila::network::neutron' do
+  shared_examples 'manila::neutron' do
+    context 'with default parameters' do
+      it 'configures manila network neutron' do
+        is_expected.to contain_manila_config('neutron/insecure').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_manila_config('neutron/auth_url').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_manila_config('neutron/auth_type').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_manila_config('neutron/cafile').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_manila_config('neutron/region_name').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_manila_config('neutron/timeout').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_manila_config('neutron/endpoint_type').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_manila_config('DEFAULT/network_plugin_ipv4_enabled').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_manila_config('DEFAULT/network_plugin_ipv6_enabled').with_value('<SERVICE DEFAULT>')
 
-  let :params do
-    {
-      :neutron_admin_username       => 'neutron',
-      :neutron_admin_password       => 'password',
-      :neutron_admin_tenant_name    => 'service',
-      :neutron_region_name          => 'nova',
-      :neutron_ca_certificates_file => '/etc/neutron/ca-certificates',
-    }
-  end
-
-  let :default_params do
-    {
-      :neutron_url                  => 'http://127.0.0.1:9696',
-      :neutron_url_timeout          => 30,
-      :neutron_admin_tenant_name    => 'service',
-      :neutron_admin_auth_url       => 'http://localhost:5000/v2.0',
-      :neutron_api_insecure         => false,
-      :neutron_auth_strategy        => 'keystone',
-      :network_plugin_ipv4_enabled  => '<SERVICE DEFAULT>',
-      :network_plugin_ipv6_enabled  => '<SERVICE DEFAULT>',
-    }
-  end
-
-  shared_examples_for 'neutron network plugin' do
-    let :params_hash do
-      default_params.merge(params)
+        # These should be added only when auth_type is 'password'
+        is_expected.not_to contain_manila_config('neutron/user_domain_name')
+        is_expected.not_to contain_manila_config('neutron/project_domain_name')
+        is_expected.not_to contain_manila_config('neutron/project_name')
+        is_expected.not_to contain_manila_config('neutron/username')
+        is_expected.not_to contain_manila_config('neutron/password')
+      end
     end
 
-    it 'configures neutron network plugin' do
+    context 'with overridden parameters' do
+      let :params do
+        {
+          :insecure       => true,
+          :auth_url       => 'http://127.0.0.2:5000/',
+          :auth_type      => 'password',
+          :cafile         => '/etc/ssl/certs/ca.crt',
+          :region_name    => 'RegionOne',
+          :timeout        => 30,
+          :endpoint_type  => 'publicURL',
+          :username       => 'neutronv1',
+          :password       => '123123',
+          :network_plugin_ipv4_enabled  => false,
+          :network_plugin_ipv6_enabled  => true,
+        }
+      end
 
-      is_expected.to contain_manila_config("DEFAULT/network_api_class").with_value(
-        'manila.network.neutron.neutron_network_plugin.NeutronNetworkPlugin')
+      it 'configures manila neutron with overridden parameters' do
+      	is_expected.to contain_manila_config('DEFAULT/network_api_class').with_value('manila.network.neutron.neutron_network_plugin.NeutronNetworkPlugin')
+        is_expected.to contain_manila_config('neutron/insecure').with_value(true)
+        is_expected.to contain_manila_config('neutron/auth_url').with_value('http://127.0.0.2:5000/')
+        is_expected.to contain_manila_config('neutron/auth_type').with_value('password')
+        is_expected.to contain_manila_config('neutron/cafile').with_value('/etc/ssl/certs/ca.crt')
+        is_expected.to contain_manila_config('neutron/user_domain_name').with_value('Default')
+        is_expected.to contain_manila_config('neutron/project_domain_name').with_value('Default')
+        is_expected.to contain_manila_config('neutron/project_name').with_value('service')
+        is_expected.to contain_manila_config('neutron/region_name').with_value('RegionOne')
+        is_expected.to contain_manila_config('neutron/timeout').with_value(30)
+        is_expected.to contain_manila_config('neutron/endpoint_type').with_value('publicURL')
+        is_expected.to contain_manila_config('neutron/username').with_value('neutronv1')
+        is_expected.to contain_manila_config('neutron/password').with_value('123123').with_secret(true)
+        is_expected.to contain_manila_config('DEFAULT/network_plugin_ipv4_enabled').with_value(false)
+        is_expected.to contain_manila_config('DEFAULT/network_plugin_ipv6_enabled').with_value(true)
+       end
+    end
 
-      params_hash.each_pair do |config,value|
-        is_expected.to contain_manila_config("DEFAULT/#{config}").with_value( value )
+    context 'with deprecated parameters' do
+      let :params do
+        {
+          :auth_type                       => 'password',
+          :neutron_api_insecure            => true,
+          :neutron_ca_certificates_file    => '/foo/ssl/certs/ca.crt',
+          :neutron_admin_tenant_name       => 'service2',
+          :neutron_admin_username          => 'neutronv2',
+          :neutron_admin_password          => '321321',
+          :neutron_url_timeout             => 30,
+        }
+      end
+
+      it 'configures manila compute nova with deprecated parameters' do
+        is_expected.to contain_manila_config('neutron/auth_type').with_value('password')
+        is_expected.to contain_manila_config('neutron/insecure').with_value(true)
+        is_expected.to contain_manila_config('neutron/cafile').with_value('/foo/ssl/certs/ca.crt')
+        is_expected.to contain_manila_config('neutron/project_name').with_value('service2')
+        is_expected.to contain_manila_config('neutron/username').with_value('neutronv2')
+        is_expected.to contain_manila_config('neutron/password').with_value('321321')
+        is_expected.to contain_manila_config('neutron/timeout').with_value(30)
       end
     end
   end
-
-
 
   on_supported_os({
     :supported_os => OSDefaults.get_supported_os
@@ -50,17 +91,8 @@ describe 'manila::network::neutron' do
       let (:facts) do
         facts.merge!(OSDefaults.get_facts({ :fqdn => 'some.host.tld'}))
       end
-      context 'with default parameters' do
-        before do
-          params = {}
-        end
-        it_configures 'neutron network plugin'
-      end
 
-      context 'with provided parameters' do
-        it_configures 'neutron network plugin'
-      end
+      it_behaves_like 'manila::neutron'
     end
   end
-
 end
