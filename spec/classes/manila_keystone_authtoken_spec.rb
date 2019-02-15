fa35dc12a94eee58c413cc9d7b433994cda7bb0d
ZhongShengping <chdzsp@163.com>
2019-02-15 10:03:06 +0800

    Service_token_roles_required missing in the server config file
    Service_token_roles_required missing in the server config file which
    allows backwards compatibility to ensure that the service tokens are
    compared against a list of possible roles for validity.
    
    Change-Id: Ic46519231da4dd54c139176d25292e0b15609c26
    Closes-Bug: 1778198

--- spec/classes/manila_keystone_authtoken_spec.rb
+++ spec/classes/manila_keystone_authtoken_spec.rb
@@ -47,6 +47,7 @@ describe 'manila::keystone::authtoken' do
         is_expected.to contain_manila_config('keystone_authtoken/memcached_servers').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_manila_config('keystone_authtoken/region_name').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_manila_config('keystone_authtoken/token_cache_time').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_manila_config('keystone_authtoken/service_token_roles_required').with_value('<SERVICE DEFAULT>')
       end
     end
 
@@ -85,6 +86,7 @@ describe 'manila::keystone::authtoken' do
           :manage_memcache_package              => true,
           :region_name                          => 'region2',
           :token_cache_time                     => '301',
+          :service_token_roles_required         => false,
         })
       end
 
@@ -120,6 +122,7 @@ describe 'manila::keystone::authtoken' do
         is_expected.to contain_manila_config('keystone_authtoken/memcached_servers').with_value('memcached01:11211,memcached02:11211')
         is_expected.to contain_manila_config('keystone_authtoken/region_name').with_value(params[:region_name])
         is_expected.to contain_manila_config('keystone_authtoken/token_cache_time').with_value(params[:token_cache_time])
+        is_expected.to contain_manila_config('keystone_authtoken/service_token_roles_required').with_value(params[:service_token_roles_required])
       end
 
       it 'installs python memcache package' do
