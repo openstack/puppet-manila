f0ca1f7ecbc3561910e696ee307728cd6487768e
ZhongShengping <chdzsp@163.com>
2019-04-28 15:00:10 +0800

    Deprecate idle_timeout option
    The idle_timeout parameter is deprecated, use connection_recycle_time
    instead[1].
    
    [1]https://review.opendev.org/#/c/334182/
    
    Change-Id: I5833ab59c724954de44c0d51d41de35f2ec53736
    Depends-On: https://review.opendev.org/656106/
    Closes-Bug: #1826692

--- manifests/db.pp
+++ manifests/db.pp
@@ -8,7 +8,7 @@
 #   Url used to connect to database.
 #   (Optional) Defaults to 'sqlite:////var/lib/manila/manila.sqlite'.
 #
-# [*database_idle_timeout*]
+# [*database_connection_recycle_time*]
 #   Timeout when db connections should be reaped.
 #   (Optional) Defaults to $::os_service_default
 #
@@ -42,24 +42,37 @@
 #   before error is raised. Set to -1 to specify an infinite retry count.
 #   Defaults to $::os_service_default
 #
+# DEPRECATED PARAMETERS
+#
+# [*database_idle_timeout*]
+#   Timeout when db connections should be reaped.
+#   Defaults to undef.
+#
 class manila::db (
-  $database_connection     = 'sqlite:////var/lib/manila/manila.sqlite',
-  $database_idle_timeout   = $::os_service_default,
-  $database_min_pool_size  = $::os_service_default,
-  $database_max_pool_size  = $::os_service_default,
-  $database_max_retries    = $::os_service_default,
-  $database_retry_interval = $::os_service_default,
-  $database_max_overflow   = $::os_service_default,
-  $database_pool_timeout   = $::os_service_default,
-  $database_db_max_retries = $::os_service_default,
+  $database_connection              = 'sqlite:////var/lib/manila/manila.sqlite',
+  $database_connection_recycle_time = $::os_service_default,
+  $database_min_pool_size           = $::os_service_default,
+  $database_max_pool_size           = $::os_service_default,
+  $database_max_retries             = $::os_service_default,
+  $database_retry_interval          = $::os_service_default,
+  $database_max_overflow            = $::os_service_default,
+  $database_pool_timeout            = $::os_service_default,
+  $database_db_max_retries          = $::os_service_default,
+  # DEPRECATED PARAMETERS
+  $database_idle_timeout            = undef,
 ) {
 
   include ::manila::deps
 
+  if $database_idle_timeout {
+    warning('The database_idle_timeout parameter is deprecated. Please use \
+database_connection_recycle_time instead.')
+  }
+
   # NOTE(spredzy): In order to keep backward compatibility we rely on the pick function
   # to use manila::<myparam> if manila::db::<myparam> isn't specified.
   $database_connection_real = pick($::manila::sql_connection, $database_connection)
-  $database_idle_timeout_real = pick($::manila::sql_idle_timeout, $database_idle_timeout)
+  $database_connection_recycle_time_real = pick($::manila::sql_idle_timeout, $database_idle_timeout, $database_connection_recycle_time)
   $database_min_pool_size_real = pick($::manila::database_min_pool_size, $database_min_pool_size)
   $database_max_pool_size_real = pick($::manila::database_max_pool_size, $database_max_pool_size)
   $database_max_retries_real = pick($::manila::database_max_retries, $database_max_retries)
@@ -70,14 +83,14 @@ class manila::db (
     ['^(sqlite|mysql(\+pymysql)?|postgresql):\/\/(\S+:\S+@\S+\/\S+)?'])
 
   oslo::db { 'manila_config':
-    connection     => $database_connection_real,
-    idle_timeout   => $database_idle_timeout_real,
-    min_pool_size  => $database_min_pool_size_real,
-    max_pool_size  => $database_max_pool_size_real,
-    max_retries    => $database_max_retries_real,
-    retry_interval => $database_retry_interval_real,
-    max_overflow   => $database_max_overflow_real,
-    pool_timeout   => $database_pool_timeout,
-    db_max_retries => $database_db_max_retries,
+    connection              => $database_connection_real,
+    connection_recycle_time => $database_connection_recycle_time_real,
+    min_pool_size           => $database_min_pool_size_real,
+    max_pool_size           => $database_max_pool_size_real,
+    max_retries             => $database_max_retries_real,
+    retry_interval          => $database_retry_interval_real,
+    max_overflow            => $database_max_overflow_real,
+    pool_timeout            => $database_pool_timeout,
+    db_max_retries          => $database_db_max_retries,
   }
 }
--- spec/classes/manila_db_spec.rb
+++ spec/classes/manila_db_spec.rb
@@ -6,45 +6,45 @@ describe 'manila::db' do
       it { should contain_class('manila::deps') }
 
       it { should contain_oslo__db('manila_config').with(
-        :db_max_retries => '<SERVICE DEFAULT>',
-        :connection     => 'sqlite:////var/lib/manila/manila.sqlite',
-        :idle_timeout   => '<SERVICE DEFAULT>',
-        :min_pool_size  => '<SERVICE DEFAULT>',
-        :max_pool_size  => '<SERVICE DEFAULT>',
-        :max_retries    => '<SERVICE DEFAULT>',
-        :retry_interval => '<SERVICE DEFAULT>',
-        :max_overflow   => '<SERVICE DEFAULT>',
-        :pool_timeout   => '<SERVICE DEFAULT>',
+        :db_max_retries          => '<SERVICE DEFAULT>',
+        :connection              => 'sqlite:////var/lib/manila/manila.sqlite',
+        :connection_recycle_time => '<SERVICE DEFAULT>',
+        :min_pool_size           => '<SERVICE DEFAULT>',
+        :max_pool_size           => '<SERVICE DEFAULT>',
+        :max_retries             => '<SERVICE DEFAULT>',
+        :retry_interval          => '<SERVICE DEFAULT>',
+        :max_overflow            => '<SERVICE DEFAULT>',
+        :pool_timeout            => '<SERVICE DEFAULT>',
       )}
     end
 
     context 'with specific parameters' do
       let :params do
         {
-          :database_connection     => 'mysql+pymysql://manila:manila@localhost/manila',
-          :database_idle_timeout   => '3601',
-          :database_min_pool_size  => '2',
-          :database_max_pool_size  => '21',
-          :database_max_retries    => '11',
-          :database_max_overflow   => '21',
-          :database_pool_timeout   => '21',
-          :database_retry_interval => '11',
-          :database_db_max_retries => '-1',
+          :database_connection              => 'mysql+pymysql://manila:manila@localhost/manila',
+          :database_connection_recycle_time => '3601',
+          :database_min_pool_size           => '2',
+          :database_max_pool_size           => '21',
+          :database_max_retries             => '11',
+          :database_max_overflow            => '21',
+          :database_pool_timeout            => '21',
+          :database_retry_interval          => '11',
+          :database_db_max_retries          => '-1',
         }
       end
 
       it { should contain_class('manila::deps') }
 
       it { should contain_oslo__db('manila_config').with(
-        :db_max_retries => '-1',
-        :connection     => 'mysql+pymysql://manila:manila@localhost/manila',
-        :idle_timeout   => '3601',
-        :min_pool_size  => '2',
-        :max_pool_size  => '21',
-        :max_retries    => '11',
-        :retry_interval => '11',
-        :max_overflow   => '21',
-        :pool_timeout   => '21',
+        :db_max_retries          => '-1',
+        :connection              => 'mysql+pymysql://manila:manila@localhost/manila',
+        :connection_recycle_time => '3601',
+        :min_pool_size           => '2',
+        :max_pool_size           => '21',
+        :max_retries             => '11',
+        :retry_interval          => '11',
+        :max_overflow            => '21',
+        :pool_timeout            => '21',
       )}
     end
   end
