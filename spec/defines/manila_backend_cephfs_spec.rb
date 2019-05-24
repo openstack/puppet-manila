4ca7fdce4ec1971ea10e58c96402f5561c3dee25
Giulio Fidente <gfidente@redhat.com>
2019-05-24 14:41:12 +0200

    Add parameters necessary to use a remote Ganesha service
    The CephFS backend can use SSH to configure Ganesha even when this
    is deployed on remote servers. This change adds the parameters
    necessary to configure Manila with the appropriate credentials to
    connect via SSH to the remote server.
    
    Change-Id: I31fff6a1b6fede860b8aaee0079183924e8b31d6
    (cherry picked from commit 80a8df65930fee17ed5c736e966f06242ff7e065)

--- spec/defines/manila_backend_cephfs_spec.rb
+++ spec/defines/manila_backend_cephfs_spec.rb
@@ -6,15 +6,18 @@ describe 'manila::backend::cephfs' do
     let(:title) {'cephfs'}
     let :params do
       {
-        :driver_handles_share_servers => false,
-        :share_backend_name           => 'cephfs',
-        :cephfs_conf_path             => '$state_path/ceph.conf',
-        :cephfs_auth_id               => 'manila',
-        :cephfs_cluster_name          => 'ceph',
-        :cephfs_enable_snapshots      => true,
-        :cephfs_protocol_helper_type  => 'NFS',
-        :cephfs_ganesha_server_ip     => '10.0.0.1',
-        :cephfs_volume_mode           => '0775'
+        :driver_handles_share_servers       => false,
+        :share_backend_name                 => 'cephfs',
+        :cephfs_conf_path                   => '$state_path/ceph.conf',
+        :cephfs_auth_id                     => 'manila',
+        :cephfs_cluster_name                => 'ceph',
+        :cephfs_enable_snapshots            => true,
+        :cephfs_protocol_helper_type        => 'NFS',
+        :cephfs_ganesha_server_ip           => '10.0.0.1',
+        :cephfs_ganesha_server_is_remote    => true,
+        :cephfs_ganesha_server_username     => 'ganeshadmin',
+        :cephfs_ganesha_path_to_private_key => '/readable/by/manila.key',
+        :cephfs_volume_mode                 => '0775'
       }
     end
 
@@ -36,7 +39,13 @@ describe 'manila::backend::cephfs' do
       is_expected.to contain_manila_config('cephfs/cephfs_ganesha_server_ip').with_value(
         '10.0.0.1')
       is_expected.to contain_manila_config('cephfs/cephfs_volume_mode').with_value(
-        '0775'
+        '0775')
+      is_expected.to contain_manila_config('cephfs/cephfs_ganesha_server_is_remote').with_value(
+        true)
+      is_expected.to contain_manila_config('cephfs/cephfs_ganesha_server_username').with_value(
+        'ganeshadmin')
+      is_expected.to contain_manila_config('cephfs/cephfs_ganesha_path_to_private_key').with_value(
+        '/readable/by/manila.key'
       )
     end
 
