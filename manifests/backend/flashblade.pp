727562e5573c6988302628ce6adfcad9f9b7c436
Simon Dodsley <simon@purestorage.com>
2019-08-09 18:33:56 -0400

    Support for Pure Storage flashBlade Manila driver
    This change adds a new define for manila::backend::flashblade,
    allowing users to create multiple backend definitions within their puppet
    manifests for use with Manila.
    
    Depends-On: I8de380bca1b55d4d0ee44a5e5d052a7dced467df
    Change-Id: I08c52c42916909e6689564d20bc778dfaf0f23ff

---
+++ manifests/backend/flashblade.pp
@@ -0,0 +1,67 @@
+# == define: manila::backend::flashblade
+#
+# Configures Manila to use the Pure Storage FlashBlade share driver
+# Compatible for multiple backends
+#
+# === Parameters
+#
+# [*flashblade_api*]
+#   (required) API token for admin-privileged user on system.
+#
+# [*flashblade_mgmt_vip*]
+#   (required) Management VIP for the FlashBlade.
+#
+# [*flashblade_data_vip*]
+#   (required) Data VIP for the FlashBlade.
+#
+# [*share_backend_name*]
+#   (optional) Name of the backend in manila.conf that
+#   these settings will reside in
+#
+# [*flashblade_eradicate*]
+#   (optional) Fully eradicate deleted shares and snapshots.
+#   Defaults to True
+#
+# [*package_ensure*]
+#   (optional) Ensure state for package. Defaults to 'present'.
+#
+# === Examples
+#
+#  manila::backend::flashblade { 'myBackend':
+#    flashblade_api                => <API token>,
+#    flashblade_mgmt_vip           => <Management VIP of the FlashBlade>,
+#    flashblade_data_vip           => <Data VIP address of FlashBlade>,
+#  }
+#
+define manila::backend::flashblade (
+  $flashblade_api,
+  $flashblade_data_vip,
+  $flashblade_mgmt_vip,
+  $flashblade_eradicate = true,
+  $share_backend_name   = $name,
+  $package_ensure       = 'present',
+) {
+
+  include manila::deps
+
+  validate_legacy(String, 'validate_string', $flashblade_api)
+
+  $flashblade_share_driver = 'manila.share.drivers.purestorage.flashblade.FlashBladeShareDriver'
+
+  manila_config {
+    "${share_backend_name}/share_driver":                 value => $flashblade_share_driver;
+    "${share_backend_name}/driver_handles_share_servers": value => false;
+    "${share_backend_name}/flashblade_eradicate":         value => $flashblade_eradicate;
+    "${share_backend_name}/flashblade_api":               value => $flashblade_api, secret => true;
+    "${share_backend_name}/flashblade_mgmt_vip":          value => $flashblade_mgmt_vip;
+    "${share_backend_name}/flashblade_data_vip":          value => $flashblade_data_vip;
+    "${share_backend_name}/share_backend_name":           value => $share_backend_name;
+  }
+
+  ensure_resource('package','nfs-utils',{
+    ensure => $package_ensure,
+    tag    => 'manila-support-package',
+  })
+
+}
+
