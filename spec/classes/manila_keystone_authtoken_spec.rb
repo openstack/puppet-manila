b92f8a9238b981f18f88540adcd3957efd4bdc81
Lewis Denny <ldenny@redhat.com>
2020-07-14 22:16:10 +1000

    Add support for the interface parameter in authtoken middleware
    This patch adds support for [keystone_authtoken] interface parameter,
    so that operators can define which endpoint should be used by authtoken
    middleware.
    
    Change-Id: I71ef2b5831420a59a3ef0fb9dbad672c27734c1d

--- spec/classes/manila_keystone_authtoken_spec.rb
+++ spec/classes/manila_keystone_authtoken_spec.rb
@@ -49,6 +49,7 @@ describe 'manila::keystone::authtoken' do
         is_expected.to contain_manila_config('keystone_authtoken/token_cache_time').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_manila_config('keystone_authtoken/service_token_roles').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_manila_config('keystone_authtoken/service_token_roles_required').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_manila_config('keystone_authtoken/interface').with_value('<SERVICE DEFAULT>')
       end
     end
 
@@ -89,6 +90,7 @@ describe 'manila::keystone::authtoken' do
           :token_cache_time                     => '301',
           :service_token_roles                  => ['service'],
           :service_token_roles_required         => false,
+          :interface                            => 'internal',
         })
       end
 
@@ -126,6 +128,7 @@ describe 'manila::keystone::authtoken' do
         is_expected.to contain_manila_config('keystone_authtoken/token_cache_time').with_value(params[:token_cache_time])
         is_expected.to contain_manila_config('keystone_authtoken/service_token_roles').with_value(params[:service_token_roles])
         is_expected.to contain_manila_config('keystone_authtoken/service_token_roles_required').with_value(params[:service_token_roles_required])
+        is_expected.to contain_manila_config('keystone_authtoken/interface').with_value(params[:interface])
       end
 
       it 'installs python memcache package' do
