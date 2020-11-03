c590c8cd270be570354d7c33a342f4d2fd3bf58a
Takashi Kajinami <tkajinam@redhat.com>
2020-11-03 17:16:54 +0900

    Add support for the keystone_authtoken/service_type parameter
    Change-Id: I579b00d8be841a7bd75f28b21e0beacb04d2ff1e
    (cherry picked from commit 43e06eb24077fc10ad821c431cfe8b525eebac68)

--- spec/classes/manila_keystone_authtoken_spec.rb
+++ spec/classes/manila_keystone_authtoken_spec.rb
@@ -49,6 +49,7 @@ describe 'manila::keystone::authtoken' do
         is_expected.to contain_manila_config('keystone_authtoken/token_cache_time').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_manila_config('keystone_authtoken/service_token_roles').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_manila_config('keystone_authtoken/service_token_roles_required').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_manila_config('keystone_authtoken/service_type').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_manila_config('keystone_authtoken/interface').with_value('<SERVICE DEFAULT>')
       end
     end
@@ -90,6 +91,7 @@ describe 'manila::keystone::authtoken' do
           :token_cache_time                     => '301',
           :service_token_roles                  => ['service'],
           :service_token_roles_required         => false,
+          :service_type                         => 'identity',
           :interface                            => 'internal',
         })
       end
@@ -128,6 +130,7 @@ describe 'manila::keystone::authtoken' do
         is_expected.to contain_manila_config('keystone_authtoken/token_cache_time').with_value(params[:token_cache_time])
         is_expected.to contain_manila_config('keystone_authtoken/service_token_roles').with_value(params[:service_token_roles])
         is_expected.to contain_manila_config('keystone_authtoken/service_token_roles_required').with_value(params[:service_token_roles_required])
+        is_expected.to contain_manila_config('keystone_authtoken/service_type').with_value(params[:service_type])
         is_expected.to contain_manila_config('keystone_authtoken/interface').with_value(params[:interface])
       end
 
