cf3eda1a4ac85bdc71566b9502699b849a4d00aa
Takashi Kajinami <tkajinam@redhat.com>
2020-12-29 16:19:10 +0900

    Use yaml instead of json for policy file
    Because usage of json for policy file will be deprecated and replaced
    by yaml[1].
    
    [1] https://governance.openstack.org/tc/goals/selected/wallaby/migrate-policy-format-from-json-to-yaml.html
    
    Depends-on: https://review.opendev.org/769647
    Change-Id: I5990be7fb911e89b4a54bcc4776c8227cb36a926

--- manifests/policy.pp
+++ manifests/policy.pp
@@ -20,12 +20,12 @@
 #   Defaults to empty hash.
 #
 # [*policy_path*]
-#   (Optional) Path to the manila policy.json file
-#   Defaults to /etc/manila/policy.json
+#   (Optional) Path to the manila policy.yaml file
+#   Defaults to /etc/manila/policy.yaml
 #
 class manila::policy (
   $policies    = {},
-  $policy_path = '/etc/manila/policy.json',
+  $policy_path = '/etc/manila/policy.yaml',
 ) {
 
   include manila::deps
@@ -34,9 +34,10 @@ class manila::policy (
   validate_legacy(Hash, 'validate_hash', $policies)
 
   Openstacklib::Policy::Base {
-    file_path  => $policy_path,
-    file_user  => 'root',
-    file_group => $::manila::params::group,
+    file_path   => $policy_path,
+    file_user   => 'root',
+    file_group  => $::manila::params::group,
+    file_format => 'yaml',
   }
 
   create_resources('openstacklib::policy::base', $policies)
