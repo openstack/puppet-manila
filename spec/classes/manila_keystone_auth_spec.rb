01341d4862f9d3945db8d0ecd8388f9e56d6b1d4
Takashi Kajinami <tkajinam@redhat.com>
2021-09-27 22:08:04 +0900

    Do not test detail of keystone::resource::service_identity
    ... but test interfaces of that resource type. This helps us avoid
    direct breakage caused by any change in puppet-keystone.
    
    Change-Id: Ib2f116f3a0fbfaf63217e05f94fca16e085e0a0d

--- spec/classes/manila_keystone_auth_spec.rb
+++ spec/classes/manila_keystone_auth_spec.rb
@@ -1,126 +1,122 @@
+#
+# Unit tests for manila::keystone::auth
+#
+
 require 'spec_helper'
 
 describe 'manila::keystone::auth' do
-
-  let :params do
-    {:password    => 'pw',
-     :password_v2 => 'pw2'}
-  end
-
   shared_examples_for 'manila::keystone::auth' do
-    context 'with only required params' do
-
-      it 'should contain auth info' do
-
-        is_expected.to contain_keystone_user('manila').with(
-          :ensure   => 'present',
-          :password => 'pw',
-          :email    => 'manila@localhost',
-        )
-        is_expected.to contain_keystone_user_role('manila@services').with(
-          :ensure  => 'present',
-          :roles   => ['admin']
-        )
-        is_expected.to contain_keystone_service('manila::share').with(
-          :ensure      => 'present',
-          :description => 'Manila Service'
-        )
-
-        is_expected.to contain_keystone_user('manilav2').with(
-          :ensure   => 'present',
-          :password => 'pw2',
-          :email    => 'manilav2@localhost',
-        )
-        is_expected.to contain_keystone_user_role('manilav2@services').with(
-          :ensure  => 'present',
-          :roles   => ['admin']
-        )
-        is_expected.to contain_keystone_service('manilav2::sharev2').with(
-          :ensure      => 'present',
-          :description => 'Manila Service v2'
-        )
-
+    context 'with default class parameters' do
+      let :params do
+        { :password => 'manila_password' }
       end
-      it { is_expected.to contain_keystone_endpoint('RegionOne/manila::share').with(
-        :ensure       => 'present',
-        :public_url   => 'http://127.0.0.1:8786/v1/%(tenant_id)s',
-        :admin_url    => 'http://127.0.0.1:8786/v1/%(tenant_id)s',
-        :internal_url => 'http://127.0.0.1:8786/v1/%(tenant_id)s'
-      ) }
-      it { is_expected.to contain_keystone_endpoint('RegionOne/manilav2::sharev2').with(
-        :ensure       => 'present',
-        :public_url   => 'http://127.0.0.1:8786/v2',
-        :admin_url    => 'http://127.0.0.1:8786/v2',
-        :internal_url => 'http://127.0.0.1:8786/v2'
+
+      it { is_expected.to contain_keystone__resource__service_identity('manila').with(
+        :configure_user      => true,
+        :configure_user_role => true,
+        :configure_endpoint  => true,
+        :service_name        => 'manila',
+        :service_type        => 'share',
+        :service_description => 'Manila Service',
+        :region              => 'RegionOne',
+        :auth_name           => 'manila',
+        :password            => 'manila_password',
+        :email               => 'manila@localhost',
+        :tenant              => 'services',
+        :public_url          => 'http://127.0.0.1:8786/v1/%(tenant_id)s',
+        :internal_url        => 'http://127.0.0.1:8786/v1/%(tenant_id)s',
+        :admin_url           => 'http://127.0.0.1:8786/v1/%(tenant_id)s',
       ) }
 
+      it { is_expected.to contain_keystone__resource__service_identity('manilav2').with(
+        :configure_user      => true,
+        :configure_user_role => true,
+        :configure_endpoint  => true,
+        :service_name        => 'manilav2',
+        :service_type        => 'sharev2',
+        :service_description => 'Manila Service v2',
+        :region              => 'RegionOne',
+        :auth_name           => 'manilav2',
+        :password            => 'manila_password',
+        :email               => 'manilav2@localhost',
+        :tenant              => 'services',
+        :public_url          => 'http://127.0.0.1:8786/v2',
+        :internal_url        => 'http://127.0.0.1:8786/v2',
+        :admin_url           => 'http://127.0.0.1:8786/v2',
+      ) }
     end
 
-    context 'when overriding endpoint parameters' do
-      before do
-        params.merge!(
-          :region          => 'RegionThree',
-          :public_url      => 'https://10.0.42.1:4242/v42/%(tenant_id)s',
-          :admin_url       => 'https://10.0.42.2:4242/v42/%(tenant_id)s',
-          :internal_url    => 'https://10.0.42.3:4242/v42/%(tenant_id)s',
-          :public_url_v2   => 'https://10.0.42.1:4242/v43',
-          :admin_url_v2    => 'https://10.0.42.2:4242/v43',
-          :internal_url_v2 => 'https://10.0.42.3:4242/v43'
-        )
+    context 'when overriding parameters' do
+      let :params do
+        { :password               => 'manila_password',
+          :auth_name              => 'alt_manila',
+          :email                  => 'alt_manila@alt_localhost',
+          :tenant                 => 'alt_service',
+          :configure_endpoint     => false,
+          :service_description    => 'Alternative Manila Service',
+          :service_name           => 'alt_service',
+          :service_type           => 'alt_share',
+          :region                 => 'RegionTwo',
+          :public_url             => 'https://10.10.10.10:80',
+          :internal_url           => 'http://10.10.10.11:81',
+          :admin_url              => 'http://10.10.10.12:81',
+          :password_v2            => 'manilav2_password',
+          :auth_name_v2           => 'alt_manilav2',
+          :email_v2               => 'alt_manilav2@alt_localhost',
+          :configure_endpoint_v2  => false,
+          :service_description_v2 => 'Alternative Manila Service v2',
+          :service_name_v2        => 'alt_servicev2',
+          :service_type_v2        => 'alt_sharev2',
+          :public_url_v2          => 'https://10.10.10.20:80',
+          :internal_url_v2        => 'http://10.10.10.21:81',
+          :admin_url_v2           => 'http://10.10.10.22:81' }
       end
 
-      it { is_expected.to contain_keystone_endpoint('RegionThree/manila::share').with(
-        :ensure       => 'present',
-        :public_url   => 'https://10.0.42.1:4242/v42/%(tenant_id)s',
-        :admin_url    => 'https://10.0.42.2:4242/v42/%(tenant_id)s',
-        :internal_url => 'https://10.0.42.3:4242/v42/%(tenant_id)s'
-      )}
-      it { is_expected.to contain_keystone_endpoint('RegionThree/manilav2::sharev2').with(
-        :ensure       => 'present',
-        :public_url   => 'https://10.0.42.1:4242/v43',
-        :admin_url    => 'https://10.0.42.2:4242/v43',
-        :internal_url => 'https://10.0.42.3:4242/v43'
-      )}
-    end
+      it { is_expected.to contain_keystone__resource__service_identity('manila').with(
+        :configure_user      => true,
+        :configure_user_role => true,
+        :configure_endpoint  => false,
+        :service_name        => 'alt_service',
+        :service_type        => 'alt_share',
+        :service_description => 'Alternative Manila Service',
+        :region              => 'RegionTwo',
+        :auth_name           => 'alt_manila',
+        :password            => 'manila_password',
+        :email               => 'alt_manila@alt_localhost',
+        :tenant              => 'alt_service',
+        :public_url          => 'https://10.10.10.10:80',
+        :internal_url        => 'http://10.10.10.11:81',
+        :admin_url           => 'http://10.10.10.12:81',
+      ) }
 
-    context 'when endpoint should not be configured' do
-      before do
-        params.merge!(
-          :configure_endpoint    => false,
-          :configure_endpoint_v2 => false
-        )
-      end
-      it { is_expected.to_not contain_keystone_endpoint('RegionOne/manila::share') }
-      it { is_expected.to_not contain_keystone_endpoint('RegionOne/manilav2::sharev2') }
-    end
-
-    context 'when overriding service names' do
-
-      before do
-        params.merge!(
-          :service_name    => 'manila_service',
-          :service_name_v2 => 'manila_service_v2',
-        )
-      end
-
-      it { is_expected.to contain_keystone_user('manila') }
-      it { is_expected.to contain_keystone_user_role('manila@services') }
-      it { is_expected.to contain_keystone_service('manila_service::share') }
-      it { is_expected.to contain_keystone_service('manila_service_v2::sharev2') }
-      it { is_expected.to contain_keystone_endpoint('RegionOne/manila_service::share') }
-      it { is_expected.to contain_keystone_endpoint('RegionOne/manila_service_v2::sharev2') }
-
+      it { is_expected.to contain_keystone__resource__service_identity('manilav2').with(
+        :configure_user      => true,
+        :configure_user_role => true,
+        :configure_endpoint  => false,
+        :service_name        => 'alt_servicev2',
+        :service_type        => 'alt_sharev2',
+        :service_description => 'Alternative Manila Service v2',
+        :region              => 'RegionTwo',
+        :auth_name           => 'alt_manilav2',
+        :password            => 'manilav2_password',
+        :email               => 'alt_manilav2@alt_localhost',
+        :tenant              => 'alt_service',
+        :public_url          => 'https://10.10.10.20:80',
+        :internal_url        => 'http://10.10.10.21:81',
+        :admin_url           => 'http://10.10.10.22:81',
+      ) }
     end
   end
+
   on_supported_os({
     :supported_os => OSDefaults.get_supported_os
   }).each do |os,facts|
     context "on #{os}" do
       let (:facts) do
-        facts.merge!(OSDefaults.get_facts({ :fqdn => 'some.host.tld'}))
+        facts.merge!(OSDefaults.get_facts())
       end
+
       it_behaves_like 'manila::keystone::auth'
     end
   end
-
 end
