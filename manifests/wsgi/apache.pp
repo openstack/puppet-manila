6653fe35c0967435b3ee25034e034dffc44bd4eb
Takashi Kajinami <tkajinam@redhat.com>
2021-11-03 20:56:19 +0900

    Prepare to update default of <service>::wsgi::apache::ssl
    Currently the <service>::wsgi::apache::ssl parameters have inconsistent
    default values. Some parameters default to true while the other default
    to false.
    
    Based on the following points, false is considered to be the more
    reasonable default.
     - Usage of SSL is optional and is not always required
     - There are other methods(like load-balancer) to implement SSL
       termination
     - Enabling SSL doesn't work with the default values currently
       defined, and requires additional parameters like ssl_cert.
     - false is the default value defined in the base implementation in
       puppet-openstacklib.
    
    This change is the preparation to change the default value, and
    introduces a warning message to make users aware of the future change.
    
    Change-Id: I52810b8340912ccb12b522d8837128bc78907b57

--- manifests/wsgi/apache.pp
+++ manifests/wsgi/apache.pp
@@ -110,7 +110,7 @@ class manila::wsgi::apache (
   $port                        = 8786,
   $bind_host                   = undef,
   $path                        = '/',
-  $ssl                         = true,
+  $ssl                         = undef,
   $workers                     = $::os_workers,
   $ssl_cert                    = undef,
   $ssl_key                     = undef,
@@ -129,11 +129,16 @@ class manila::wsgi::apache (
   $vhost_custom_fragment       = undef,
 ) {
 
+  if $ssl == undef {
+    warning('Default of the ssl parameter will be changed in a future release')
+  }
+  $ssl_real = pick($ssl, true)
+
   include manila::deps
   include manila::params
   include apache
   include apache::mod::wsgi
-  if $ssl {
+  if $ssl_real {
     include apache::mod::ssl
   }
 
@@ -144,7 +149,7 @@ class manila::wsgi::apache (
     path                        => $path,
     priority                    => $priority,
     servername                  => $servername,
-    ssl                         => $ssl,
+    ssl                         => $ssl_real,
     ssl_ca                      => $ssl_ca,
     ssl_cert                    => $ssl_cert,
     ssl_certs_dir               => $ssl_certs_dir,
