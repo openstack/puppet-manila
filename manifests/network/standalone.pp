7deaf777352fc5022806442a9026fc6165d26590
Takashi Kajinami <tkajinam@redhat.com>
2022-03-19 00:09:10 +0900

    Fix the wrong section used to configure standalone network plugin
    This change fixes the wrong section used by manila::network::standalone
    to define the network_plugin_ipvN_enabled parameters.
    Previously this usage resulted in duplicate resource declaration, and
    made this resource type unusable.
    
    This change also replaces usage of undef by $::os_service_default.
    These two are effectively same but $::os_service_default is now
    globally used.
    
    (victoria to ussuri)
    Conflicts:
    	manifests/network/standalone.pp
    
    Closes-Bug: #1965548
    Change-Id: Iedd0805ed815f0507d8fe7b7c91afdb5dfc320be
    (cherry picked from commit b903e954ede6e2f6ca72923e67a9f8821bab4225)
    (cherry picked from commit 55bb672cb16616e327ee7834666976a04d05aafd)
    (cherry picked from commit 3fadcaaee9c10facf38d3a30267878aaab534a73)
    (cherry picked from commit d5cbf5c1d89323eb287c47ad9213139af1d66a9a)

--- manifests/network/standalone.pp
+++ manifests/network/standalone.pp
@@ -16,6 +16,7 @@
 # It will be assigned to share-network and share drivers will be
 # able to use this for network interfaces within provisioned
 # share servers. Optional. Example: 1001
+# Defaults to $::os_service_default
 #
 # [*standalone_network_plugin_allowed_ip_ranges*]
 # (optional) Can be IP address, range of IP addresses or list of addresses
@@ -24,12 +25,15 @@
 # addresses from network can be used. Optional.
 # Examples: 10.0.0.10 or 10.0.0.10-10.0.0.20 or
 # 10.0.0.10-10.0.0.20,10.0.0.30-10.0.0.40,10.0.0.50
+# Defaults to $::os_service_default
 #
 # [*network_plugin_ipv4_enabled*]
 # (optional) Whether to support Ipv4 network resource
+# Defaults to $::os_service_default
 #
 # [*network_plugin_ipv6_enabled*]
 # (optional) whether to support IPv6 network resource
+# Defaults to $::os_service_default
 #
 # DEPRECATED PARAMETERS
 #
@@ -41,12 +45,12 @@
 define manila::network::standalone (
   $standalone_network_plugin_gateway,
   $standalone_network_plugin_mask,
-  $standalone_network_plugin_segmentation_id    = undef,
-  $standalone_network_plugin_allowed_ip_ranges  = undef,
-  $network_plugin_ipv4_enabled                  = $::os_service_default,
-  $network_plugin_ipv6_enabled                  = $::os_service_default,
+  $standalone_network_plugin_segmentation_id   = $::os_service_default,
+  $standalone_network_plugin_allowed_ip_ranges = $::os_service_default,
+  $network_plugin_ipv4_enabled                 = $::os_service_default,
+  $network_plugin_ipv6_enabled                 = $::os_service_default,
   # DEPRECATED PARAMETERS
-  $standalone_network_plugin_ip_version         = undef,
+  $standalone_network_plugin_ip_version        = undef,
 ) {
 
   if $standalone_network_plugin_ip_version {
@@ -56,13 +60,12 @@ define manila::network::standalone (
   $standalone_plugin_name = 'manila.network.standalone_network_plugin.StandaloneNetworkPlugin'
 
   manila_config {
-    "${name}/network_api_class":                            value => $standalone_plugin_name;
-    "${name}/standalone_network_plugin_gateway":            value => $standalone_network_plugin_gateway;
-    "${name}/standalone_network_plugin_mask":               value => $standalone_network_plugin_mask;
-    "${name}/standalone_network_plugin_segmentation_id":    value => $standalone_network_plugin_segmentation_id;
-    "${name}/standalone_network_plugin_allowed_ip_ranges":  value => $standalone_network_plugin_allowed_ip_ranges;
-    'DEFAULT/network_plugin_ipv4_enabled':                  value => $network_plugin_ipv4_enabled;
-    'DEFAULT/network_plugin_ipv6_enabled':                  value => $network_plugin_ipv6_enabled;
-
+    "${name}/network_api_class":                           value => $standalone_plugin_name;
+    "${name}/standalone_network_plugin_gateway":           value => $standalone_network_plugin_gateway;
+    "${name}/standalone_network_plugin_mask":              value => $standalone_network_plugin_mask;
+    "${name}/standalone_network_plugin_segmentation_id":   value => $standalone_network_plugin_segmentation_id;
+    "${name}/standalone_network_plugin_allowed_ip_ranges": value => $standalone_network_plugin_allowed_ip_ranges;
+    "${name}/network_plugin_ipv4_enabled":                 value => $network_plugin_ipv4_enabled;
+    "${name}/network_plugin_ipv6_enabled":                 value => $network_plugin_ipv6_enabled;
   }
 }
