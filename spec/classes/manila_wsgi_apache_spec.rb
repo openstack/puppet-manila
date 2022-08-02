42344e5722704fd725bcc4182bdec63de0fe1c26
Takashi Kajinami <tkajinam@redhat.com>
2022-08-02 20:35:08 +0900

    Adapt to new type validation in puppetlabs-apache
    The puppetlabs-apache module is enforcing more strict data type
    validation[1].
    
    This change updates the default values to adapt to that change.
    
    [1] https://github.com/puppetlabs/puppetlabs-apache/commit/f41251e336fa3e7c31c19968ccee74121cf71e47
    
    Closes-Bug: #1983300
    Depends-on: https://review.opendev.org/851652
    Change-Id: I690b84be0d91e39dc44b2013d90bf364710eac07

--- spec/classes/manila_wsgi_apache_spec.rb
+++ spec/classes/manila_wsgi_apache_spec.rb
@@ -9,6 +9,7 @@ describe 'manila::wsgi::apache' do
         :bind_port                   => 8786,
         :group                       => 'manila',
         :path                        => '/',
+        :priority                    => 10,
         :servername                  => facts[:fqdn],
         :ssl                         => false,
         :threads                     => 1,
@@ -22,8 +23,8 @@ describe 'manila::wsgi::apache' do
         :headers                     => nil,
         :request_headers             => nil,
         :custom_wsgi_process_options => {},
-        :access_log_file             => false,
-        :access_log_format           => false,
+        :access_log_file             => nil,
+        :access_log_format           => nil,
       )}
     end
 
