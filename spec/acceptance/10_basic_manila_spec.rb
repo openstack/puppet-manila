b6fd20f77a69ad932ec5e349ddae5014f28fcae0
Takashi Kajinami <tkajinam@redhat.com>
2022-08-17 13:00:37 +0900

    Ubuntu: Use utf8mb3_general_ci collate in MySQL
    Currently idempotency in Ubuntu is broken because of the below change
    detected in collate in MySQL.
    
    ```
    /Stage[main]/Keystone::Db::Mysql/Openstacklib::Db::Mysql[keystone]/
    Mysql_database[keystone]/collate: collate changed 'utf8mb3_general_ci'
    to 'utf8_general_ci'
    ```
    
    Similarly to what we observed in the past about charset[1], it seems
    MySQL in Ubuntu is automatically converting the collate value and that
    is causing the "unexpected" change detected in the 2nd puppet run.
    
    This fixes the idempotency by using utf8mb3_general_ci in Ubuntu to
    avoid the mismatch caused by internal translation.
    
    [1] f29a3051fc097759e82149e8ea21cffd8c67225d
    
    Depends-on: https://review.opendev.org/851775
    Change-Id: I0fddef098e72d515ef746af56b98bbf190896214

--- spec/acceptance/10_basic_manila_spec.rb
+++ spec/acceptance/10_basic_manila_spec.rb
@@ -40,6 +40,7 @@ describe 'basic manila' do
       }
       class { 'manila::db::mysql':
         charset  => $::openstack_integration::params::mysql_charset,
+        collate  => $::openstack_integration::params::mysql_collate,
         password => 'a_big_secret',
       }
       class { 'manila::keystone::auth':
