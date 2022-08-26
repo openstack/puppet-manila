b36bb510da99be157634776b44029b236e9d6bd9
Takashi Kajinami <tkajinam@redhat.com>
2022-08-26 10:25:37 +0900

    Remove deprecated glusterfs_native_path_to_private_key
    ... because it was deprecated during Wallaby cycle[1].
    
    [1] bacfbc1c661a089c879fb66b1a965231254a9e0d
    
    Change-Id: Ic7c6b80ca3e80d216f65a1075851280663ce1c16

--- manifests/backend/glusternative.pp
+++ manifests/backend/glusternative.pp
@@ -4,15 +4,6 @@
 # Configures Manila to use GlusterFS native as a share driver
 #
 # === Parameters
-# [*share_backend_name*]
-#   (optional) Name of the backend in manila.conf that
-#   these settings will reside in
-#
-# [*backend_availability_zone*]
-#   (Optional) Availability zone for this share backend.
-#   If not set, the storage_availability_zone option value
-#   is used as the default for all backends.
-#   Defaults to $::os_service_default.
 #
 # [*glusterfs_servers*]
 #   (required) List of GlusterFS servers that can be used to create shares.
@@ -26,25 +17,26 @@
 #   (required) Regular expression template used to filter GlusterFS volumes for
 #   share creation.
 #
+# [*share_backend_name*]
+#   (optional) Name of the backend in manila.conf that
+#   these settings will reside in
+#
+# [*backend_availability_zone*]
+#   (Optional) Availability zone for this share backend.
+#   If not set, the storage_availability_zone option value
+#   is used as the default for all backends.
+#   Defaults to $::os_service_default.
+#
 # [*package_ensure*]
 #   (optional) Ensure state for package. Defaults to 'present'.
-#
-# DEPRECATED PARAMETERS
-#
-# [*glusterfs_native_path_to_private_key*]
-#   (optional) Path of Manila host's private SSH key file. This parameter has
-#   been replaced by glusterfs_path_to_private_key. Compatibility will be
-#   removed in a future release.
 #
 define manila::backend::glusternative (
   $glusterfs_servers,
   $glusterfs_volume_pattern,
-  $share_backend_name                   = $name,
-  $backend_availability_zone            = $::os_service_default,
-  $package_ensure                       = 'present',
-  $glusterfs_path_to_private_key        = undef,
-  # DEPRECATED PARAMETERS
-  $glusterfs_native_path_to_private_key = undef,
+  $glusterfs_path_to_private_key,
+  $share_backend_name             = $name,
+  $backend_availability_zone      = $::os_service_default,
+  $package_ensure                 = 'present',
 ) {
 
   include manila::deps
@@ -52,18 +44,12 @@ define manila::backend::glusternative (
 
   $share_driver = 'manila.share.drivers.glusterfs_native.GlusterfsNativeShareDriver'
 
-  if $glusterfs_native_path_to_private_key {
-    warning('The glusterfs_native_path_to_private_key parameter is deprecated, use glusterfs_path_to_private_key instead')
-  }
-
-  $glusterfs_path_to_private_key_real = pick($glusterfs_path_to_private_key, $glusterfs_native_path_to_private_key)
-
   manila_config {
     "${share_backend_name}/share_backend_name":            value => $share_backend_name;
     "${share_backend_name}/backend_availability_zone":     value => $backend_availability_zone;
     "${share_backend_name}/share_driver":                  value => $share_driver;
     "${share_backend_name}/glusterfs_servers":             value => $glusterfs_servers;
-    "${share_backend_name}/glusterfs_path_to_private_key": value => $glusterfs_path_to_private_key_real;
+    "${share_backend_name}/glusterfs_path_to_private_key": value => $glusterfs_path_to_private_key;
     "${share_backend_name}/glusterfs_volume_pattern":      value => $glusterfs_volume_pattern;
   }
 
--- spec/defines/manila_backend_glusternative_spec.rb
+++ spec/defines/manila_backend_glusternative_spec.rb
@@ -25,30 +25,6 @@ describe 'manila::backend::glusternative' do
         end
       end
     end
-
-
-    context 'with deprecated parameters' do
-      let :params do
-        {
-          :glusterfs_servers                    => 'remoteuser@volserver',
-          :glusterfs_native_path_to_private_key => '/etc/glusterfs/glusterfs.pem',
-          :glusterfs_volume_pattern             => 'manila-share-volume-\d+$',
-        }
-      end
-
-      it 'configures glusternative share driver with deprecated parameters' do
-        is_expected.to contain_manila_config('fuse/share_backend_name').with(
-          :value => 'fuse')
-        is_expected.to contain_manila_config('fuse/share_driver').with_value(
-          'manila.share.drivers.glusterfs_native.GlusterfsNativeShareDriver')
-        is_expected.to contain_manila_config('fuse/glusterfs_servers').with_value(
-          'remoteuser@volserver')
-        is_expected.to contain_manila_config('fuse/glusterfs_path_to_private_key').with_value(
-          '/etc/glusterfs/glusterfs.pem')
-        is_expected.to contain_manila_config('fuse/glusterfs_volume_pattern').with_value(
-          'manila-share-volume-\d+$')
-      end
-    end
   end
 
   on_supported_os({
