11e2e6a7c751dd703b08d67951e188d64834e220
Takashi Kajinami <tkajinam@redhat.com>
2022-11-28 16:07:08 +0900

    Support manila-data service
    Manila provides the separate manila-data service to process
    host-assisted share migration. This change introduces support for
    the service.
    
    Change-Id: I4ed63ae3992735352fd0f5a317e998074e127127

---
+++ spec/classes/manila_data_spec.rb
@@ -0,0 +1,99 @@
+require 'spec_helper'
+
+describe 'manila::data' do
+
+  shared_examples_for 'manila::data' do
+
+    context 'with default parameters' do
+
+      it { is_expected.to contain_class('manila::params') }
+
+      it { is_expected.to contain_manila_config('DEFAULT/mount_tmp_location').with_value('<SERVICE DEFAULT>') }
+      it { is_expected.to contain_manila_config('DEFAULT/check_hash').with_value('<SERVICE DEFAULT>') }
+
+      it { is_expected.to contain_service('manila-data').with(
+        :name      => platform_params[:data_service],
+        :enable    => true,
+        :ensure    => 'running',
+        :hasstatus => true,
+        :tag       => 'manila-service',
+      ) }
+    end
+
+    context 'with parameters' do
+      let :params do
+        {
+          :mount_tmp_location => '/tmp/',
+          :check_hash         => false,
+        }
+      end
+
+      it { is_expected.to contain_manila_config('DEFAULT/mount_tmp_location').with_value('/tmp/') }
+      it { is_expected.to contain_manila_config('DEFAULT/check_hash').with_value(false) }
+    end
+
+    context 'with manage_service false' do
+      let :params do
+        {
+          :manage_service => false
+        }
+      end
+      it 'should not configure the service' do
+        is_expected.to_not contain_service('manila-data')
+      end
+    end
+  end
+
+  shared_examples_for 'manila::data on Debian' do
+    context 'with default parameters' do
+      it { is_expected.to contain_package('manila-data').with(
+        :name      => 'manila-data',
+        :ensure    => 'present',
+        :tag       => ['openstack', 'manila-package'],
+      ) }
+    end
+
+    context 'with parameters' do
+      let :params do
+        {
+          :package_ensure => 'installed'
+        }
+      end
+
+      it { is_expected.to contain_package('manila-data').with(
+        :name      => 'manila-data',
+        :ensure    => 'installed',
+        :tag       => ['openstack', 'manila-package'],
+      ) }
+    end
+  end
+
+  shared_examples_for 'manila::data on RedHat' do
+    context 'with default parameters' do
+      it { is_expected.to_not contain_package('manila-data') }
+    end
+  end
+
+  on_supported_os({
+    :supported_os => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge!(OSDefaults.get_facts())
+      end
+
+      let :platform_params do
+        case facts[:osfamily]
+        when 'Debian'
+          { :data_service => 'manila-data' }
+        when 'RedHat'
+          { :data_service => 'openstack-manila-data' }
+        end
+      end
+
+      it_behaves_like 'manila::data'
+      it_behaves_like "manila::data on #{facts[:osfamily]}"
+    end
+  end
+
+end
