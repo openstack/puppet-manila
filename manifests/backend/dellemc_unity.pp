815ab987c9b3e4f9fc13a912bcc513483aedb543
Takashi Kajinami <kajinamit@oss.nttdata.com>
2023-10-21 18:50:57 +0900

    Allow multiple Unity backend
    The current implementation causes duplicated resources when multiple
    Unity backends exist.
    
    Change-Id: Ia364951d989a78d05327fd2f2bc64173426382fa
    (cherry picked from commit d00ac4804b67b2f9f821a20ea90a67b72a2076b8)

--- manifests/backend/dellemc_unity.pp
+++ manifests/backend/dellemc_unity.pp
@@ -141,8 +141,8 @@ define manila::backend::dellemc_unity (
   })
 
   # Python library storops is required to run Unity driver.
-  package{'storops':
+  ensure_packages( 'storops', {
     ensure   => $package_ensure,
     provider => 'pip',
-  }
+  })
 }
