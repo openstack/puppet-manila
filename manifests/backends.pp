5cc6faada5f57d57b3c3c5c5b166c5e65dc697a5
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-03-28 15:02:02 +0900

    Require enabled_share_backends
    If manila-share launches without enabled_share_backends, it actually
    has no effect because it can't manage any share backend.
    
    Require a valid inputs to detect potential misconfiguration.
    
    Change-Id: I9d4387384908384e9b7d2b1621f856f49bce7cd8

--- manifests/backends.pp
+++ manifests/backends.pp
@@ -6,12 +6,12 @@
 #
 # [*enabled_share_backends*]
 #   (Required) a list of ini sections to enable.
-#   This should contain names used in ceph::backend::* resources.
+#   This should contain names used in manila::backend::* resources.
 #   Example: ['share1', 'share2', 'sata3']
 #
 # Author: Andrew Woodward <awoodward@mirantis.com>
 class manila::backends (
-  $enabled_share_backends = undef
+  Variant[String[1], Array[String[1], 1]] $enabled_share_backends
 ) {
 
   include manila::deps
